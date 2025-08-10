24c7712fca2d51eb67bae626891775de73e25b89
yongckim <zxcv9203@naver.com>
2025-08-10 22:14:45 +0900

    :building_construction: build : web 모듈 의존성 및 Version Catalog에 MockK 추가

--- modules/web/build.gradle.kts
+++ modules/web/build.gradle.kts
@@ -5,4 +5,5 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(libs.mockk)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "local-talk-BE"
 include("apps:localtalk-api")
 include("modules:jpa")
+include("modules:web")
 include("supports:logging")
