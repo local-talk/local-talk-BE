671d486da9ef5d1680b4f4a07c09ca9b40fe7d31
yongckim <zxcv9203@naver.com>
2025-08-10 13:37:19 +0900

    :tada: init : web 모듈 생성

---
+++ modules/web/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation(project(":supports:logging"))
+    
+    api("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
