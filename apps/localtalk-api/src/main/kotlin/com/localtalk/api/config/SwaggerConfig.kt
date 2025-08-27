cd6e09adf6eca49d01cb671138bf5ee5d838e8f7
yongckim <zxcv9203@naver.com>
2025-08-27 19:16:47 +0900

    :wrench: config : JWT 인증이 필요한 API에 대해 인증 강제 추가

--- apps/localtalk-api/src/main/kotlin/com/localtalk/api/config/SwaggerConfig.kt
+++ apps/localtalk-api/src/main/kotlin/com/localtalk/api/config/SwaggerConfig.kt
@@ -1,7 +1,10 @@
 package com.localtalk.api.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,6 +20,25 @@ class SwaggerConfig {
                     .title("Local Talk API")
                     .description("Local Talk 백엔드 API 문서")
                     .version("1.0.0"),
+            )
+            .addSecurityItem(
+                SecurityRequirement().addList("bearerAuth"),
+            )
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "bearerAuth",
+                        SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                            .description("JWT 토큰을 입력하세요 (Bearer 제외)"),
+                    ),
+            )
+            .addServersItem(
+                Server()
+                    .url("https://localtalk.site")
+                    .description("운영 서버"),
             )
             .addServersItem(
                 Server()
