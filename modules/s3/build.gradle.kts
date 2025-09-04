569dc9cb610910309f85ed8682a8a2621ea60134
yongckim <zxcv9203@naver.com>
2025-09-05 00:11:00 +0900

    :wrench: config : s3 사용을 위한 의존성 및 파일 업로드 관련 설정 추가

--- modules/s3/build.gradle.kts
+++ modules/s3/build.gradle.kts
@@ -5,10 +5,12 @@ plugins {
 dependencies {
     api(libs.spring.cloud.aws.starter.s3)
     api(libs.spring.boot.configuration.processor)
+    // multipart file 사용을 위해 추가
+    api("org.springframework:spring-web")
 
     testImplementation(libs.spring.boot.starter.test)
     testImplementation(libs.testcontainers.localstack)
-    
+
     testFixturesImplementation(libs.spring.boot.starter.test)
     testFixturesImplementation(libs.spring.test)
     testFixturesImplementation(libs.spring.cloud.aws.testcontainers)
--- modules/s3/src/main/kotlin/com/localtalk/s3/config/S3Config.kt
+++ modules/s3/src/main/kotlin/com/localtalk/s3/config/S3Config.kt
@@ -1,8 +1,35 @@
 package com.localtalk.s3.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import java.net.URI
 
 @Configuration
 @EnableConfigurationProperties(S3Properties::class)
-class S3Config
+class S3Config {
+
+    @Bean
+    @Profile("local")
+    fun localStackS3Client(
+        @Value("\${spring.cloud.aws.s3.endpoint:http://localhost:4566}") endpoint: String,
+        @Value("\${storage.s3.region:us-east-1}") region: String
+    ): S3Client {
+        return S3Client.builder()
+            .endpointOverride(URI.create(endpoint))
+            .region(Region.of(region))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create("test", "test")
+                )
+            )
+            .forcePathStyle(true)
+            .build()
+    }
+}
