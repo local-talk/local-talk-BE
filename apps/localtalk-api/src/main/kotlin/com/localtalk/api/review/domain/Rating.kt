ffd413c1031491a33d9100bc6b3a7d63de87cd30
jiixon <jr0107380@gmail.com>
2025-09-07 14:51:52 +0900

    :recycle: refactor: review의 rating과 comment init 블록에서 검증하도록 수정

--- apps/localtalk-api/src/main/kotlin/com/localtalk/api/review/domain/Rating.kt
+++
@@ -1,3 +0,0 @@
-package com.localtalk.api.review.domain
-
-data class Rating()
--- apps/localtalk-api/src/main/kotlin/com/localtalk/api/review/domain/Review.kt
+++ apps/localtalk-api/src/main/kotlin/com/localtalk/api/review/domain/Review.kt
@@ -5,6 +5,7 @@ import com.localtalk.api.member.domain.Member
 import com.localtalk.domain.SoftDeleteBaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.ConstraintMode
+import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.ForeignKey
@@ -31,4 +32,12 @@ class Review(
     var comment: String? = null,
 
 ) : SoftDeleteBaseEntity(){
+
+    init {
+        require(rating in 1..5) { "별점은 1~5 사이여야 합니다." }
+
+        comment?.let {
+            require(it.length <= 300) { "댓글은 300자 이하여야 합니다." }
+        }
+    }
 }
