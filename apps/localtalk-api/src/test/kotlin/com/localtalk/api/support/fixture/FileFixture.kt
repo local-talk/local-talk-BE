593d122806745ed54f689b896babff0ab9f25c9b
yongckim <zxcv9203@naver.com>
2025-09-09 21:07:01 +0900

    :recycle: refactor : Multipart 파일 데이터를 Fixture로 분리하고 file 업로드 API 직접 호출하도록 변경

--- apps/localtalk-api/src/test/kotlin/com/localtalk/api/support/fixture/FileFixture.kt
+++ apps/localtalk-api/src/test/kotlin/com/localtalk/api/support/fixture/FileFixture.kt
@@ -1,6 +1,10 @@
 package com.localtalk.api.support.fixture
 
+import org.springframework.http.HttpEntity
+import org.springframework.http.MediaType
+import org.springframework.http.client.MultipartBodyBuilder
 import org.springframework.mock.web.MockMultipartFile
+import org.springframework.util.MultiValueMap
 
 object FileFixture {
 
@@ -14,4 +18,20 @@ object FileFixture {
         contentType,
         ByteArray(size.toInt()),
     )
+
+    fun createMultipartData(
+        file: MockMultipartFile,
+        type: String
+    ): MultiValueMap<String, HttpEntity<*>> {
+        val bodyBuilder = MultipartBodyBuilder()
+
+        bodyBuilder.part("file", file.resource)
+            .filename(file.originalFilename)
+            .contentType(MediaType.parseMediaType(file.contentType!!))
+
+        bodyBuilder.part("type", type)
+            .contentType(MediaType.TEXT_PLAIN)
+
+        return bodyBuilder.build()
+    }
 }
